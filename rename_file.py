26ec4f64c49f442d139778a9d88bb5c90a2d3638
Cao Thế Anh <76893527+caoanh112@users.noreply.github.com>
2021-06-19 13:32:04 +0700

    Add files via upload

--- rename_file.py
+++ rename_file.py
@@ -1,5 +1,7 @@
 import xlrd
 import os
+import re
+import sys
 
 
 ls = ''
@@ -8,12 +10,50 @@ while ls.endswith('.xlsx') == False:
 	ls = input()
 print ('Nhập đường dẫn folder chứa các file cần đổi tên (ex: C:\ danhsach\ bailam\ ): ')
 url = input()
+
+# ls = '/home/caoanh/CODE/rename/rename_file/list.xlsx'
+# url = '/home/caoanh/CODE/rename/tieuluan/'
 wb = xlrd.open_workbook(ls)
 sheet = wb.sheet_by_index(0)
 print (sheet.cell_value(1, 1))
 print (sheet.nrows)
 print (sheet.ncols)
 print ('============================')
+
+
+patterns = {
+    '[àáảãạăắằẵặẳâầấậẫẩ]': 'a',
+    '[đ]': 'd',
+    '[èéẻẽẹêềếểễệ]': 'e',
+    '[ìíỉĩị]': 'i',
+    '[òóỏõọôồốổỗộơờớởỡợ]': 'o',
+    '[ùúủũụưừứửữự]': 'u',
+    '[ỳýỷỹỵ]': 'y'
+}
+
+#"Họ Và Tên" ==>> "Ho_Va_Ten"
+def convert(text):
+    output = text
+    for regex, replace in patterns.items():
+        output = re.sub(regex, replace, output)
+        # deal with upper case
+        output = re.sub(regex.upper(), replace.upper(), output)
+    return output
+
+
+
+def format(text):
+	print (text)
+	print ('______________________________________')
+	output = ""
+	for i in range(0,len(text)):
+		if text[i] == ' ':
+			output += '_'
+		else:
+			output += text[i]
+	return output
+
+
 
 #split student id from 'filename.pdf' 
 def ID_student(str):
@@ -28,6 +68,8 @@ def ID_student(str):
 			print(ID)
 		i+=1
 	return ID
+
+
 #get name student from 'list.xlsx' by ID
 def Get_name(ID):
 	col = 1
@@ -35,23 +77,22 @@ def Get_name(ID):
 	for row in range(0,sheet.nrows):
 		print (row)
 		if ID == sheet.cell_value(row, col):
-			name = sheet.cell_value(row, col + 1) + " " + sheet.cell_value(row, col +2)
+			name = sheet.cell_value(row, col + 1) + "_" + sheet.cell_value(row, col +2)
 			print (name)
 			break
 	return name
 
 
 def main():
-	for count, filename in enumerate(os.listdir(url)):
 
-		# check file type
+	for count, filename in enumerate(os.listdir(url)):
 		if filename.endswith('.pdf') == True:
 			file_type = '.pdf'
 		if filename.endswith('.docx') == True:
 			file_type = '.docx'
-
 		ID = ID_student(filename)
-		name = Get_name(ID)
+		name = format(convert(Get_name(ID)))
+		print (name)
 		dst = ID + "-" + name + file_type
 		src = url + filename
 		dst = url + dst
